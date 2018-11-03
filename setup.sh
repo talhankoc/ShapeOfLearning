e7f99a65af41fb4940eef47dbd0cb192f4a158b8
Kunaal Sharma <kss45@duke.edu>
2018-11-03 17:20:02 -0400

    added setup file

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#Updates all packages, sets up git, clones repo, installs julia
+GITEMAIL="kss45@duke.edu"
+GITUSER="Kunaal Sharma"
+GITREPO="https://github.com/talhankoc/ShapeOfLearning.git"
+GITPATH="/home/ec2-user/"
+
+cd ${GITPATH}
+echo "y" | sudo yum update
+echo "y" | sudo yum install git-core
+echo "y" | sudo yum install python-devel
+echo "y" | sudo yum install gcc
+sudo pip install numpy
+sudo pip install matplotlib
+git config --global user.name ${GITUSER}
+git config --global user.email ${GITEMAIL}
+git clone ${GITREPO}
+sudo tar -xvzf "ShapeOfLearning/Binaries/julia-1.0.1-linux-x86_64.tar.gz"
+sudo PATH=${PATH}:${GITPATH}/julia-1.0.1/bin/
+sudo export $PATH
